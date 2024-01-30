ec85ecdebf81f58002a8260f6ab69a87db0b5f4e
quanndm <minhquan1852@gmail.com>
2024-01-30 21:49:25 +0700

    update step ingestion

--- data-ingestion.py
+++ data-ingestion.py
@@ -2,15 +2,15 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('data-ingestion').getOrCreate()
 
-
+my_folder_path = 'hdfs:///user/hadoop/my_file'
 df = spark.read\
     .option('header','true')\
     .option('inferSchema','true')\
-    .csv('/user/hadoop/my_folder/project/uber-data-analytics/uber-data.csv')
+    .csv(f'{my_folder_path}/uber-data.csv')
 
 data_lake_dir = 'hdfs:///datalake/uber-data-analytics'
 # save file to hdfs
-df.write.mode('overwrite').parquet(f'{data_lake_dir}/raw_data/uber_data.csv')
+df.write.mode('overwrite').parquet(f'{data_lake_dir}/raw_data/uber_data.parquet')
 
 # end section
 print("ingest raw data to data lake!")
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Uber Data Analytics project
+##  Tech stack
+- in coming...
+## demo
+1. step: data ingestion
+
+![Run spark submit in step ingestion](./resource/step1.1.png "command when run spark-submit")
+![Run spark submit in step ingestion](./resource/step1.2.png "result after run command")
